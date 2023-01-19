7474344436f2b705fb742ec1ec60178519172f54
Jerry <47369872+jerryh-jr@users.noreply.github.com>
2023-01-19 15:33:32 -0500

    5: Move global components into Local
    Lesson 5 Global vs Local Components

--- component-fundaments-coffee/plan-picker/app.js
+++ component-fundaments-coffee/plan-picker/app.js
@@ -1,18 +1,22 @@
-const app = Vue.createApp({
-})
-.component('plan-picker', {
+let PlanComponent = {
+    template: '#plan-template',
+    props:{
+        name: { type: String, required: true },
+    }
+};
+
+let PlanPickerComponent = {
+    components: { plan: PlanComponent },
     template: '#plan-picker-template',
     data() {
         return {
             plans: ['The Single', 'The Curious', 'The Addict']
         }
     }
+};
+
+const app = Vue.createApp({
+    components: { 'PlanPicker': PlanPickerComponent }
 })
 
-.component('plan', {
-    template: '#plan-template',
-    props:{
-        name: { type: String, required: true },
-    }
-})
 .mount('#app')
